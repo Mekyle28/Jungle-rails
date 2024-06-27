34a524e8036bc65a17e422f2407d44c3613ddf2e
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-06-27 00:23:58 -0400

    Update the order page to render the line-items using the line-item partial

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,11 @@
 class Product < ApplicationRecord
   monetize :price_cents, numericality: true
   mount_uploader :image, ProductImageUploader
-
+  
+  has_many :line_items
   belongs_to :category
+  
+  
 
   validates :name, presence: true
   validates :price, presence: true
