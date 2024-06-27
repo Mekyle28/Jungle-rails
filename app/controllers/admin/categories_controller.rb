54e2f82d656c69f116c4fbbf5164b9f63a493604
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-06-27 18:47:29 -0400

    initialize categories controller and add methods for index, new, and cerate

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,22 @@
+class Admin::CategoriesController < Admin::BaseController
+
+  def index
+    @categories = Category.order(id: :desc).all
+
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(name: params[:category][:name])
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+end
