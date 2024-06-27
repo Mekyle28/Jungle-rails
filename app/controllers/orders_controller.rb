72eb2f6714b799efd3d241fa524b436cd7108027
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-06-27 00:21:00 -0400

    update active record request to join the line-items and the products tables

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,9 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
-    @line_item = LineItem.where(order_id: params[:id])
+    @line_items = LineItem.joins(:product).where(order_id: params[:id])
+    end
+    
   end
 
   def create
