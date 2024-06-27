26e7a6488ebefe729b649d6a7552f7af9dd15652
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-06-27 12:52:04 -0400

    add instance variables that use ar to get the total # of products and catagories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,10 @@
-class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: ENV['USERNAME'], password:ENV['PASSWORD']
+class Admin::DashboardController < Admin::BaseController
+  
   
   def show
+    @total_products = Product.count
+    @total_categories = Category.count
+    puts @total_products
+    puts @total_categories
   end
 end
