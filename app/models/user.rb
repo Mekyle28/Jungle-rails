ca7045f3d457152730003480122e045a83cd4f16
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-07-01 21:27:00 -0400

    add validations to require name, email, min password length to user model and add method to authenticate users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,29 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, presence: true, uniqueness: true
+  def self.authenticate_with_credentials(email, password)
+    trimmed_email = email.strip.downcase
+    user = User.find_by_email(trimmed_email)
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
+
+
+  before_validation :downcase_email
+
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 5 }
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :password_confirmation, presence: true
+
+  private
+
+  def downcase_email
+    self.email = email.downcase if email.present?
+  end 
 
 end
